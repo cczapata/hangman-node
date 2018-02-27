5dd76bcafcb489316008fb3f41d5d94b3737114e
cczapata <zapatacynthia@gmail.com>
2018-02-27 10:39:21 -0800

    update

---
+++ word.js
@@ -0,0 +1,20 @@
+var hangman;
+
+function Hangman(){
+  this.words =["Bob Dylan", "Neil young","America", "Willie Nelson"];
+  this.randWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 5;
+  this.messages = {
+    win:"You won!",
+    loose: "You lose!",
+    guessed:"Letter used",
+    notValid:"Enter a letter",
+  };
+}
+
+hangman.prototype.getWord = function () {
+  [Math.floor(Math.random()*this.words.length]
+
+};
